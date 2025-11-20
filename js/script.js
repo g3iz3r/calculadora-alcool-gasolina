c4036dd8517dd7361bb32002132b29c212c21861
g3iz3r <g3iz3r@gmail.com>
2025-11-19 21:47:32 -0300

    versao 2
    Comecei o css
    fiz o js da calculadora
    que caucula se o valor do acool gasolina for inferior a 0.73 % compensa alcool

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-let valor = Number (document.getElementById(valor).value)
-let texto = ""
-const caucular= Number
+function calcularcombustivel() {
+    const inputAlcool = document.getElementById('valorAlcool');
+    const inputGasolina = document.getElementById('valorGasolina');
 
+    const resultadoInput = document.getElementById("resultado");
+
+    const precoAlcool = parseFloat(inputAlcool.value);
+    const precoGasolina = parseFloat(inputGasolina.value);
+
+    if (isNaN(precoAlcool) || isNaN(precoGasolina) || precoAlcool <= 0 || precoGasolina <= 0) {
+        resultadoInput.value = "ERRO: Insira valores válidos.";
+    }
+
+
+    const relacao = precoAlcool / precoGasolina;
+    const FATOR_LIMITE = 0.73;
+    let mensagemResultado;
+
+    if (relacao <= FATOR_LIMITE) {
+        mensagemResultado = " Compensa abastecer com ÁLCOOL! (Etanol)";
+    } else {
+        mensagemResultado = " Compensa abastecer com GASOLINA!";
+    }
+
+    resultadoInput.innerHTML = mensagemResultado;
+}
